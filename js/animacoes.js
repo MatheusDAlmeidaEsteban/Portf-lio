efdad6daed298d70d7086e532ae2a2c0129a8054
matheus <matheusestebam123@gmail.com>
2022-03-16 22:15:27 -0300

    main

--- js/animacoes.js
+++ js/animacoes.js
@@ -44,3 +44,48 @@ animacaoTexto1(elementoH1, text1, velo);
 setTimeout(function() {
     animacaoTexto2(elementoH2, text2, velo);
 }, 3000);
+
+
+//trabalhos animações
+
+const trabalhos = document.getElementById("apresentacao-trabalhos");
+const visualizar = document.getElementById("pagina-trabalhos")
+
+trabalhos.addEventListener("click",(e)=>{
+    if(visualizar.style.visibility === "visible"){
+        visualizar.style.visibility = "hidden"
+    }
+    else{
+        visualizar.style.visibility = "visible";
+    }
+
+   
+})
+
+const trabalhos1 = document.getElementById("apresentacao-trabalhos1");
+const visualizar1 = document.getElementById("pagina-trabalhos1")
+
+trabalhos1.addEventListener("click",(e)=>{
+    if(visualizar1.style.visibility === "visible"){
+        visualizar1.style.visibility = "hidden"
+    }
+    else{
+        visualizar1.style.visibility = "visible";
+    }
+
+   
+})
+
+const trabalhos2 = document.getElementById("apresentacao-trabalhos2");
+const visualizar2 = document.getElementById("pagina-trabalhos2")
+
+trabalhos2.addEventListener("click",(e)=>{
+    if(visualizar2.style.visibility === "visible"){
+        visualizar2.style.visibility = "hidden"
+    }
+    else{
+        visualizar2.style.visibility = "visible";
+    }
+
+   
+})
